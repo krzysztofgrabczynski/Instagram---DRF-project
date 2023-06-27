455ff153c2e354b60c6d1289a19ac0d31b9da874
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-06-27 20:45:52 +0200

    Refactor: adding docstring

--- tests/user/test_models.py
+++ tests/user/test_models.py
@@ -6,13 +6,20 @@ from src.user.models import UserProfileModel
 
 
 class TestUserProfileModel(TestCase):
+    """
+    Test module for testing UserProfileModel.
+    """
     @classmethod
     def setUpTestData(cls) -> None:
         User.objects.create_user(username="test", password="test")
 
         UserProfileModel.objects.create(user=User.objects.get(username="test"))
 
-    def test_str_method(self):
+    def test_str_method(self) -> None:
+        """
+        Testing __str__() and __repr__() methods. 
+        When calling UserProfileModel object, should return a string: "Profile(id:<user_profile id>): <username>(id:<user id>)"
+        """
         user = User.objects.get(username="test")
         user_profile = UserProfileModel.objects.first()
         expected = f"Profile(id:{user_profile.id}): {user.username}(id:{user.id})"
