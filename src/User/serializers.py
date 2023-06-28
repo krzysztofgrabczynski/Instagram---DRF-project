e3d3d0c2d2b830648facc3ada9ec5680d73cda2c
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-06-28 19:43:25 +0200

    Implementation of the serializer for user registration: UserRegistrationSerializer with functionality of checking if the password and password2 are the same, checking if the email is unique, createing authentication token for new user

---
+++ src/User/serializers.py
@@ -0,0 +1,53 @@
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+
+from typing import Dict
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    """
+    Serializer for registration new user.
+    All fields are required. Username and email must be unique. Password and password2 must be the same.
+    This serializer craetes authentication token for user.
+    """
+
+    password2 = serializers.CharField(required=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password", "password2"]
+        extra_kwargs = {
+            "password": {"write_only": True},
+            "password2": {"write_only": True},
+            "email": {"required": True},
+        }
+
+    def validate(self, attrs: Dict) -> Dict:
+        if attrs["password"] is not attrs["password2"]:
+            exc_data = {"password": "Password fields must be the same"}
+            raise serializers.ValidationError(exc_data)
+
+        return super().validate(attrs)
+
+    def validate_email(self, email: str) -> str:
+        if User.objects.filter(email=email).exists():
+            raise serializers.ValidationError("User with that email already exists")
+
+        return email
+
+    def _create_user_auth_token(self, user: User) -> Token:
+        token = Token.objects.create(user=user)
+
+        return token
+
+    def craete(self, validated_data: Dict) -> User:
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            password=validated_data["password"],
+        )
+
+        self._create_user_auth_token(user)
+
+        return user
