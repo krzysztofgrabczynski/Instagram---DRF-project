8e1963f58bd93dd944a7b2ebd0e925e1a372c6a0
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-06-30 01:04:24 +0200

    Code formatting using black tool

--- src/User/urls.py
+++ src/User/urls.py
@@ -3,6 +3,4 @@ from django.urls import path, include
 from src.user import views
 
 
-urlpatterns = [
-    path("sign_up/", views.UserCreateView.as_view(), name="sign_up")
-]
+urlpatterns = [path("sign_up/", views.UserCreateView.as_view(), name="sign_up")]
