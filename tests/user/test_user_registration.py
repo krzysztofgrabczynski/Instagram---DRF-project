7019f7e82e538678a9784e37a9146ac090bce7c4
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-07-01 15:44:50 +0200

    New test group - testing invalid data

--- tests/user/test_user_registration.py
+++ tests/user/test_user_registration.py
@@ -21,6 +21,10 @@ class UserRegistrationsTestCase(TestCase):
         user_pofile_data = {"gender": 1, "description": "test_description"}
         self.data = json.dumps(dict(user=user_data, user_profile=user_pofile_data))
 
+    def byte_content_to_dict(self, byte_content: bytes) -> dict:
+        decode_content = byte_content.decode()
+        dict_content = json.loads(decode_content)
+        return dict_content
 
 class TestGenericUserRegistration(UserRegistrationsTestCase):
     """
@@ -48,33 +52,126 @@ class TestUserRegistrationPositiveResponseContent(UserRegistrationsTestCase):
         response = self.client.post(
             "/sign_up/", self.data, content_type="application/json"
         )
-        self.content = self._byte_content_to_dict(response.content)
+        self.content = self.byte_content_to_dict(response.content)
 
-    def _byte_content_to_dict(self, byte_content: bytes) -> dict:
-        decode_content = byte_content.decode()
-        dict_content = json.loads(decode_content)
-        return dict_content
-
-    def test_satus_content_username_with_valid_data(self):
+    def test_content_username_with_valid_data(self):
         self.client.post("/sign_up/", self.data, content_type="application/json")
         user_content = self.content["user"]
 
         self.assertEqual(user_content["username"], "test_username")
 
-    def test_satus_content_email_with_valid_data(self):
+    def test_content_email_with_valid_data(self):
         self.client.post("/sign_up/", self.data, content_type="application/json")
         user_content = self.content["user"]
 
         self.assertEqual(user_content["email"], "test_email@test.com")
 
-    def test_satus_content_gener_with_valid_data(self):
+    def test_content_gener_with_valid_data(self):
         self.client.post("/sign_up/", self.data, content_type="application/json")
         user_profile_content = self.content["user_profile"]
 
         self.assertEqual(user_profile_content["gender"], 1)
 
-    def test_satus_content_description_with_valid_data(self):
+    def test_content_description_with_valid_data(self):
         self.client.post("/sign_up/", self.data, content_type="application/json")
         user_profile_content = self.content["user_profile"]
 
         self.assertEqual(user_profile_content["description"], "test_description")
+
+
+class TestUserRegistrationNegativeResponseContent(
+    UserRegistrationsTestCase
+):
+    """
+    Test cases for checking response content for invalid post data.
+    """
+
+    def setUp(self) -> None:
+        self.client = APIClient()
+
+    def input_json_data(
+        self,
+        username: str = "test_username",
+        email: str = "test_email@test.com",
+        password: str = "testpassword123!",
+        password2: str = "testpassword123!",
+        gender: int = 1,
+        description: str = "test_description",
+    ) -> str:
+        user_data = {
+            "username": username,
+            "email": email,
+            "password": password,
+            "password2": password2,
+        }
+        user_pofile_data = {"gender": gender, "description": description}
+        data = json.dumps(dict(user=user_data, user_profile=user_pofile_data))
+
+        return data
+
+    def test_content_without_username(self):
+        data = self.input_json_data(username="")
+
+        response = self.client.post(
+            "/sign_up/", data, content_type="application/json"
+        )
+        self.content = self.byte_content_to_dict(response.content)
+        expected_msg = "{'user': {'username': ['This field may not be blank.']}}"
+
+        self.assertEqual(str(self.content), expected_msg)
+    
+    def test_content_without_email(self):
+        data = self.input_json_data(email="")
+
+        response = self.client.post(
+            "/sign_up/", data, content_type="application/json"
+        )
+        self.content = self.byte_content_to_dict(response.content)
+        expected_msg = "{'user': {'email': ['This field may not be blank.']}}"
+        
+        self.assertEqual(str(self.content), expected_msg)
+
+    def test_content_with_existing_email(self):
+        User.objects.create(username="test_username2", email="test_email@test.com", password="testpassword123!")
+        data = self.input_json_data()
+
+        response = self.client.post(
+            "/sign_up/", data, content_type="application/json"
+        )
+        self.content = self.byte_content_to_dict(response.content)
+        expected_msg = "{'user': {'email': ['User with that email already exists']}}"
+        
+        self.assertEqual(str(self.content), expected_msg)
+
+    def test_content_without_password(self):
+        data = self.input_json_data(password="")
+
+        response = self.client.post(
+            "/sign_up/", data, content_type="application/json"
+        )
+        self.content = self.byte_content_to_dict(response.content)
+        expected_msg = "{'user': {'password': ['This field may not be blank.']}}"
+
+        self.assertEqual(str(self.content), expected_msg)
+
+    def test_content_with_invalid_password(self):
+        data = self.input_json_data(password="1234", password2="1234")
+
+        response = self.client.post(
+            "/sign_up/", data, content_type="application/json"
+        )
+        self.content = self.byte_content_to_dict(response.content)
+        expected_msg = "{'user': {'password': ['This password is too short. It must contain at least 8 characters.', 'This password is too common.', 'This password is entirely numeric.']}}"
+        print("Content: ", self.content)
+        self.assertEqual(str(self.content), expected_msg)
+
+    def test_content_with_not_same_passwords(self):
+        data = self.input_json_data(password2="not_same_password")
+
+        response = self.client.post(
+            "/sign_up/", data, content_type="application/json"
+        )
+        self.content = self.byte_content_to_dict(response.content)
+        expected_msg = "{'user': {'password': ['Password fields must be the same']}}"
+
+        self.assertEqual(str(self.content), expected_msg)
