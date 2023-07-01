c776c0e5cb8afe04787541333b1f3a69449dc5b9
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-07-01 15:18:33 +0200

    Code refactor

--- tests/user/test_user_registration.py
+++ tests/user/test_user_registration.py
@@ -4,7 +4,11 @@ from django.contrib.auth.models import User
 import json
 
 
-class TestUserRegistration(TestCase):
+class UserRegistrationsTestCase(TestCase):
+    """
+    Generic class for user registration tests with setUp method.
+    """
+
     def setUp(self) -> None:
         self.client = APIClient()
 
@@ -17,6 +21,12 @@ class TestUserRegistration(TestCase):
         user_pofile_data = {"gender": 1, "description": "test_description"}
         self.data = json.dumps(dict(user=user_data, user_profile=user_pofile_data))
 
+
+class TestGenericUserRegistration(UserRegistrationsTestCase):
+    """
+    Generic test cases for user registration.
+    """
+
     def test_user_registration_with_valid_data(self):
         self.assertEqual(User.objects.count(), 0)
 
@@ -27,20 +37,44 @@ class TestUserRegistration(TestCase):
         self.assertEqual(response.status_code, 201)
         self.assertEqual(User.objects.count(), 1)
 
+
+class TestUserRegistrationPositiveResponseContent(UserRegistrationsTestCase):
+    """
+    Test cases for checking response content for valid post data.
+    """
+
+    def setUp(self) -> None:
+        super().setUp()
+        response = self.client.post(
+            "/sign_up/", self.data, content_type="application/json"
+        )
+        self.content = self._byte_content_to_dict(response.content)
+
     def _byte_content_to_dict(self, byte_content: bytes) -> dict:
         decode_content = byte_content.decode()
         dict_content = json.loads(decode_content)
         return dict_content
 
-    def test_satus_content_with_valid_data(self):
-        response = self.client.post(
-            "/sign_up/", self.data, content_type="application/json"
-        )
-        content = self._byte_content_to_dict(response.content)
-        user_content = content["user"]
-        user_profile_content = content["user_profile"]
+    def test_satus_content_username_with_valid_data(self):
+        self.client.post("/sign_up/", self.data, content_type="application/json")
+        user_content = self.content["user"]
 
         self.assertEqual(user_content["username"], "test_username")
+
+    def test_satus_content_email_with_valid_data(self):
+        self.client.post("/sign_up/", self.data, content_type="application/json")
+        user_content = self.content["user"]
+
         self.assertEqual(user_content["email"], "test_email@test.com")
+
+    def test_satus_content_gener_with_valid_data(self):
+        self.client.post("/sign_up/", self.data, content_type="application/json")
+        user_profile_content = self.content["user_profile"]
+
         self.assertEqual(user_profile_content["gender"], 1)
+
+    def test_satus_content_description_with_valid_data(self):
+        self.client.post("/sign_up/", self.data, content_type="application/json")
+        user_profile_content = self.content["user_profile"]
+
         self.assertEqual(user_profile_content["description"], "test_description")
