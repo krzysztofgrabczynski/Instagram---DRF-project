2703d01a9aaa8729713f27d9d2ab6d4e2bba014e
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-07-02 11:34:27 +0200

    New paths for update user data

--- src/User/urls.py
+++ src/User/urls.py
@@ -3,4 +3,8 @@ from django.urls import path, include
 from src.user import views
 
 
-urlpatterns = [path("sign_up/", views.UserCreateView.as_view(), name="sign_up")]
+urlpatterns = [
+    path("sign_up/", views.UserCreateView.as_view(), name="sign_up"),
+    path("edit_account/<int:pk>/", views.UserEditAccountView.as_view(), name="edit_account"),
+    path("edit_profile/<int:pk>/", views.UserEditProfileView.as_view(), name="edit_profile"),
+]
