fa4f872dabc4ba9e9894a05938208e1d4218fea7
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-07-02 17:37:15 +0200

    Demo version of the UserEditAccountView and UserEditProfileView

--- src/User/views.py
+++ src/User/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from rest_framework import generics
+from rest_framework import generics, viewsets, mixins
 from rest_framework.response import Response
 
-from src.user.serializers import UserRegisterSerializer
+from src.user.serializers import UserRegisterSerializer, UserSerializer, UserProfileSerializer
+from src.user.models import UserProfileModel
 
 
 class UserCreateView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserRegisterSerializer
+
+
+class UserEditAccountView(generics.UpdateAPIView):
+    pass
+
+
+class UserEditProfileView(generics.UpdateAPIView):
+    queryset = UserProfileModel.objects.all()
+    serializer_class = UserProfileSerializer
+
+
