268b24c71c827bc7365eeea17fa297070057159d
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-07-03 22:11:27 +0200

    New urls for updating user account data, password and user profile

--- src/User/urls.py
+++ src/User/urls.py
@@ -6,5 +6,6 @@ from src.user import views
 urlpatterns = [
     path("sign_up/", views.UserCreateView.as_view(), name="sign_up"),
     path("edit_account/<int:pk>/", views.UserEditAccountView.as_view(), name="edit_account"),
+    path("edit_password/<int:pk>/", views.UserEditPasswordView.as_view(), name="edit_password"),
     path("edit_profile/<int:pk>/", views.UserEditProfileView.as_view(), name="edit_profile"),
 ]
