8703786bb77877db52c85e0f456ef065437846a3
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-07-07 11:39:21 +0200

    Code formatting using black tool

--- tests/user/test_models.py
+++ tests/user/test_models.py
@@ -8,6 +8,7 @@ class TestUserProfileModel(TestCase):
     """
     Test module for testing UserProfileModel.
     """
+
     @classmethod
     def setUpTestData(cls) -> None:
         User.objects.create_user(username="test", password="test")
@@ -16,7 +17,7 @@ class TestUserProfileModel(TestCase):
 
     def test_str_method(self) -> None:
         """
-        Testing __str__() and __repr__() methods. 
+        Testing __str__() and __repr__() methods.
         When calling UserProfileModel object, should return a string: "Profile(id:<user_profile id>): <username>(id:<user id>)"
         """
         user = User.objects.get(username="test")
