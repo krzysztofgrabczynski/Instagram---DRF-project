0edd171002d0b1dc439d16d4b9392bb226438864
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-07-07 11:25:04 +0200

    New tests for user account update

--- tests/user/test_user_update.py
+++ tests/user/test_user_update.py
@@ -1,10 +1,12 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+from rest_framework.test import APIClient
 
 from src.user.models import UserProfileModel
+from tests.user.test_user_registration import UserRegistrationsTestCase as func
 
 
-class TestUserAccountUpdate(TestCase):
+class TestUserUpdateGeneric(TestCase):
     @classmethod
     def setUpTestData(cls) -> None:
         user_data = {
@@ -15,5 +17,51 @@ class TestUserAccountUpdate(TestCase):
         user = User.objects.create_user(**user_data)
         UserProfileModel.objects.create(user=user, description="test_description")
 
-    
-        
+        user_data_2 = {
+            "username": "test_user_2",
+            "email": "test_2@email.com",
+            "password": "test_password",
+        }
+        user = User.objects.create_user(**user_data_2)
+
+    def setUp(self) -> None:
+        self.client = APIClient()
+        self.user = User.objects.get(username="test_user")
+        self.user_profile = UserProfileModel.objects.get(user=self.user)
+
+
+class TestUserAccountUpdate(TestUserUpdateGeneric):
+    def test_user_account_update_with_valid_data(self):
+        response = self.client.put(
+            f"/edit_account/{self.user.id}/",
+            {"username": "test_user_update", "email": "test_update@email.com"},
+        )
+        update_user = User.objects.get(id=self.user.id)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(update_user.username, "test_user_update")
+        self.assertEqual(update_user.email, "test_update@email.com")
+
+    def test_user_account_update_with_exists_username(self):
+        response = self.client.put(
+            f"/edit_account/{self.user.id}/",
+            {"username": "test_user_2", "email": "test_update@email.com"},
+        )
+
+        content = str(response.content.decode())
+        excpected_msg = """{"username":["A user with that username already exists."]}"""
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(content, excpected_msg)
+
+    def test_user_account_update_with_exists_email(self):
+        response = self.client.put(
+            f"/edit_account/{self.user.id}/",
+            {"username": "test_user_update", "email": "test_2@email.com"},
+        )
+
+        content = str(response.content.decode())
+        excpected_msg = """{"email":["User with that email already exists"]}"""
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(content, excpected_msg)
