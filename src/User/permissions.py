616d497c6c6eb189a1a9d6c3cdae5ce996dd79f3
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-07-08 19:07:38 +0200

    Code refactor

--- src/User/permissions.py
+++ src/User/permissions.py
@@ -6,8 +6,9 @@ class UserUpdatePermission(permissions.BasePermission):
     Permission for check if the logged user can update account, profile, passowrd.
     User can update own account, profile, passowrd.
     """
+
     def has_permission(self, request, view):
         if request.user == view.get_object():
             return True
-        
+
         return False
