7eeaf47b6584d2646e17201f486062c70a666907
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-07-17 20:17:48 +0200

    Code formatting using black tool

--- src/post/serializers.py
+++ src/post/serializers.py
@@ -5,14 +5,14 @@ from src.post.models import PostModel
 
 class PostSerializer(serializers.ModelSerializer):
     """
-    Serializer for post model. 
+    Serializer for post model.
     Creates a new post by logged in user.
     User field is hidden and default set as logged in user.
     """
+
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
     class Meta:
         model = PostModel
         fields = "__all__"
-        extra_kwargs = {
-            "likes": {"read_only": True}
-        }
+        extra_kwargs = {"likes": {"read_only": True}}
--- src/post/views.py
+++ src/post/views.py
@@ -4,11 +4,16 @@ from src.post.serializers import PostSerializer
 from src.post.models import PostModel
 
 
-class PostCreateUpdateDeleteView(mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet):
+class PostCreateUpdateDeleteView(
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
     """
     View for create, update and delete specific post.
     It uses the GenericViewSet for router functionality.
     """
+
     queryset = PostModel.objects.all()
     serializer_class = PostSerializer
-    
