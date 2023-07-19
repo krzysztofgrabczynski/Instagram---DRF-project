93940489250a9c5504d8f3a5520e37b237afb103
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-07-19 17:50:59 +0200

    New view file for user profile

---
+++ src/User/user_profile_views.py
@@ -0,0 +1,22 @@
+from rest_framework import generics
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+
+from src.user.user_profile_serializers import UserProfileSerializer
+from src.user.models import UserProfileModel
+
+
+class UserProfileView(generics.RetrieveAPIView):
+    queryset = UserProfileModel.objects.all()
+    serializer_class = UserProfileSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        user = instance.user
+        nested_instance = {
+            "user_data": user,
+            "user_profile_data": instance,
+            "user_posts": user.posts,
+        }
+        serializer = self.get_serializer(nested_instance)
+        return Response(serializer.data)
