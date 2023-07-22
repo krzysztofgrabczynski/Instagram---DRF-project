ab88474efd31cba6c3a680731b48fea532f817e6
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-07-22 12:21:35 +0200

    Changing app name in apps.py using 'src' prefix

--- src/social_actions/apps.py
+++ src/social_actions/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SocialActionsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "social_actions"
+    name = "src.social_actions"
